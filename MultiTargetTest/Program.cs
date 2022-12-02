5510679a2c8f52df96b0df4244c142c6cbb2fe3a
Dmitry <dmitry@andliza.com>
2022-12-02 18:01:51 -0500

    Add files via upload

---
+++ MultiTargetTest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiTargetTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I worked!");
+        }
+    }
+}
